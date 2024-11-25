15b45f9d71fbbf7fcdaf34ec47073cc9cf318888
Martynas <martis.jascemskas@gmail.com>
2024-11-25 18:25:53 +0000

    init of EuroStats backend

---
+++ src/main/java/com/eurostats/eurostats/EuroStatsApplication.java
@@ -0,0 +1,13 @@
+package com.eurostats.eurostats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurostats/eurostats/EuroStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurostats.eurostats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
